2cff72ce230fb247569c8d2f1c31289d761bbba0
TIANQX <tian_cnsl@163.com>
2021-05-31 08:05:52 +0800

    修改打包方式为war

--- springboot-mybatis/src/main/java/com/zd/demo/DemoApplication.java
+++ springboot-mybatis/src/main/java/com/zd/demo/DemoApplication.java
@@ -3,13 +3,21 @@ package com.zd.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan({"com.zd.demo.dao"})
-public class DemoApplication {
+public class DemoApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(DemoApplication.class);
+    }
 }
